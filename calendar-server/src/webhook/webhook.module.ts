6a261e29768aef2133b3eda189f8150c7a471149
EmnaHarzallah <emna.harzallah@insat.ucar.tn>
2026-05-24 14:38:27 +0100

    implementation de webhook service signé

--- calendar-server/src/webhook/webhook.module.ts
+++ calendar-server/src/webhook/webhook.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
-import { WebhookGateway } from './webhook.gateway';
 
 @Module({
-  providers: [WebhookGateway, WebhookService],
+  providers: [WebhookService],
+  exports: [WebhookService],
 })
 export class WebhookModule {}
